18427d7b4e214c8ec787229d0a41bd5a05679608
Serramento <134259414+Serramento@users.noreply.github.com>
2023-06-21 20:45:30 +0300

    Sprint2 Challenge tamamlandı

--- index.js
+++ index.js
@@ -100,14 +100,20 @@ function cumleKur(
 /* (Oto test yok) cumleKur fonksiyonuna yalnızca 1 parametre göndererek "Hello World!" stringini elde edin, 
 sonucu konsolde gözlemleyin */
 
+console.log(cumleKur("Hello World!"));
+
 /* (Oto test yok) cumleKur fonksiyonuna yalnızca 2 parametre göndererek "Hello World!" stringini elde edin, 
 sonucu konsolde gözlemleyin */
+
+console.log(cumleKur("Hello", " World!"));
 
 /* (Oto test var) cumleKur fonksiyonuna 5 parametre göndererek "Ben iyi bir yazılımcı olacağım!" stringini 
 elde edin, sonucu `bircumle` değişkenine atayın ve konsolde gözlemleyin */
 var bircumle;
 
-/* kodlar buraya */
+birCumle = cumleKur("Ben ", "iyi ", "bir ", "yazılımcı ", "olacağım!");
+
+console.log(birCumle);
 
 //		Sayfanın en üstünde global olarak tanımlanmış `cumleler` adında bir dizi bulunmaktadır. Bu dizinin
 // içinde en çok 5 en az 1 adet string bulunan diziler bulunmaktadır.Aşağıdaki görevlerde aksi
@@ -128,9 +134,12 @@ var bircumle;
 			5. Oluşturulan yeni dizi döndürülecek.
 	*/
 
-function cumlelereDonustur(/* kodlar buraya */) {
-  /* kodlar buraya */
+function cumlelereDonustur(cumlelerDizi, ayrac = ",") {
+  const sonuc = cumlelerDizi.map((kelimeDizisi) => kelimeDizisi.join(ayrac));
+  return sonuc;
 }
+
+console.log("G1", cumlelereDonustur(cumleler," "));
 
 /* GÖREV 2:
 		paragrafOlustur fonksiyonuna aşağıdakileri uygulayın.
@@ -145,16 +154,30 @@ function cumlelereDonustur(/* kodlar buraya */) {
 			6. Oluşturulan paragraf döndürülecek
 	*/
 
-function paragrafOlustur(/* kodlar buraya */) {
-  /* kodlar buraya */
+function paragrafOlustur(cumlelerDizi, cumleKurCallback, cumlelereDonusturCallback) {
+  const yeniCumleler = cumlelereDonusturCallback(cumlelerDizi, " ");
+  return cumleKurCallback(
+    yeniCumleler[1],
+    yeniCumleler[3],
+    yeniCumleler[5],
+    yeniCumleler[7],
+    yeniCumleler[9],
+  );
 }
+
+console.log("G2", paragrafOlustur(cumleler, cumleKur, cumlelereDonustur));
 
 /* 	GÖREV 3:
 		Yukarıda isimleri sebzeler ve meyveler olan 2 dizi bulunmaktadır. Bu dizileri kullanarak aşağıdaki görevleri tamamlayın.
 			3a. meyveler dizisinin ilk ve son elemanlarını diziden çıkartın. (.pop ve .shift metodlarını kullanın)
  */
 //3a çözümü
-/* kodlar buraya */
+
+meyveler.pop();
+meyveler.shift();
+
+console.log("G3a", meyveler);
+
 
 /* 			3b.  Bir tavşan ve bir kirpi arkadaşlar sebzeler dizimizin peşine düştü. Tavşan => 🐇 , Kirpi=> 🦔 , 
 Tavşanla kirpi sebzeleri ele geçirmek için bir plan kurdular. Tavşan diziye önden saldıracak, kirpi ise 
@@ -162,15 +185,22 @@ arkalarından dolaşacak. Varsayalım ki arkadaşların planları başarılı ol
 Kirpiyi dizinin son elemanına ekleyin 🦔
  */
 //3b çözümü
-/* kodlar buraya */
+
+sebzeler.unshift("🐇");
+sebzeler.push("🦔");
+
+console.log("G3b", sebzeler);
 
 /* 			3c. manav isminde bir dizi oluşturun.`meyveler` dizisi ilk elemanlara, `sebzeler` dizisi son 
 elemanlara denk gelecek şekilde, iki diziyi birleştirip sonucu manav dizisine aktarın. (.concat metodu)
  */
 //3c çözümü
-/* kodlar buraya */
 
 var manav;
+
+manav = meyveler.concat(sebzeler);
+
+console.log("G3c", manav);
 
 /* 	GÖREV 4:
 		Yeni kurulmuş bir mesajlaşma startup firması atılan mesajları emojilerle zenginleştirmek istiyor. 
@@ -189,9 +219,20 @@ var manav;
 			4. elde edilen string döndürülecek
  */
 
-function emojileriDonustur(/* kodlar buraya */) {
-  /* kodlar buraya */
+function emojileriDonustur(mesajim, emojilerim) {
+  let yeniMesaj = mesajim;
+  let anahtarlar = Object.keys(emojilerim);
+
+  for(let i = 0; i < anahtarlar.length; i++){
+    yeniMesaj = yeniMesaj.replaceAll(anahtarlar[i].toLowerCase(), emojilerim[anahtarlar[i]]); 
+    yeniMesaj = yeniMesaj.replaceAll(anahtarlar[i].toUpperCase(), emojilerim[anahtarlar[i]]); 
+  }
+
+  return yeniMesaj;
 }
+
+console.log("G4", emojileriDonustur("Selam :) Nasılsın :D Bugünkü olay çok komikti :P ama sonra çok şaşırdık :o biraz da üzüldük :( ama yine de seviliyorsun <3", emojiler));
+
 
 /*  Bu satırın aşağısındaki kodları lütfen değiştirmeyin  */
 function sa() {
